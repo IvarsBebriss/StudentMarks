44fbde53848d975dd336cfa6282202c626ef3940
Ivars Bebrišs <ivars.bebriss@gmail.com>
2019-09-06 20:49:17 +0300

    added invoicing db

--- rekini/ltpc_pv.sql
+++ rekini/ltpc_pv.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 06, 2019 at 11:34 AM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.3.6
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 06, 2019 at 05:48 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,10 +28,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `clients`
 --
 
-CREATE TABLE `clients` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) COLLATE utf8mb4_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `clients`
@@ -39,7 +41,9 @@ CREATE TABLE `clients` (
 
 INSERT INTO `clients` (`id`, `name`) VALUES
 (1, 'Jānis'),
-(2, 'Pēteris');
+(2, 'Pēteris'),
+(3, 'Andris2'),
+(4, 'Ivars');
 
 -- --------------------------------------------------------
 
@@ -47,19 +51,49 @@ INSERT INTO `clients` (`id`, `name`) VALUES
 -- Table structure for table `invoices`
 --
 
-CREATE TABLE `invoices` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `invoices`;
+CREATE TABLE IF NOT EXISTS `invoices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `client_id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `sum` float(8,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `sum` float(8,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `invoices`
 --
 
 INSERT INTO `invoices` (`id`, `client_id`, `date`, `sum`) VALUES
-(1, 1, '2019-09-05', 2.00);
+(1, 2, '2019-09-06', 56.15),
+(2, 1, '2019-09-05', 1.24);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices_items`
+--
+
+DROP TABLE IF EXISTS `invoices_items`;
+CREATE TABLE IF NOT EXISTS `invoices_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `invoice_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` float(10,3) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `invoices_items`
+--
+
+INSERT INTO `invoices_items` (`id`, `invoice_id`, `product_id`, `quantity`) VALUES
+(1, 1, 1, 1.000),
+(2, 1, 2, 2.000),
+(3, 1, 1, 0.500),
+(4, 1, 4, 0.050),
+(7, 1, 11, 0.010),
+(8, 2, 1, 0.123);
 
 -- --------------------------------------------------------
 
@@ -67,65 +101,25 @@ INSERT INTO `invoices` (`id`, `client_id`, `date`, `sum`) VALUES
 -- Table structure for table `products`
 --
 
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8mb4_bin NOT NULL,
   `unit` varchar(10) COLLATE utf8mb4_bin NOT NULL,
-  `price` float(6,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `price` float(6,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `name`, `unit`, `price`) VALUES
-(1, 'Arbūzs', 'gab', 10.05),
+(1, 'Arbūzsa', 'gab', 10.05),
 (2, 'Lāču maize', 'gab', 2.01),
-(9, 'New company2', 'gab', 1.12);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `clients`
---
-ALTER TABLE `clients`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `invoices`
---
-ALTER TABLE `invoices`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `clients`
---
-ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `invoices`
---
-ALTER TABLE `invoices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+(3, 'New company2', 'gab', 1.12),
+(4, 'Bebra desa', 'kg', 15.00),
+(11, 'Brieža desa', 'kg', 24.00);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
